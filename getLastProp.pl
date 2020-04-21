6799eed17b127b5b1c593afb735cd494f39d3b0e
sohah <husseinsoh@gmail.com>
2020-04-21 09:39:59 -0500

    fixing the script to run on all files

--- getLastProp.pl
+++ getLastProp.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+=pod
+
 foreach (@ARGV){
     #print "file: $_\n";
     open(FILE, $_) or die "Could not read from $_, program halting.";
@@ -7,3 +9,23 @@ foreach (@ARGV){
 close FILE;
 }
 print "\n";
+=cut
+
+
+
+#!/usr/bin/perl
+
+use autodie ':io';
+foreach (@ARGV){
+
+open(my $fh, '<', $_);
+# get the data that is needed, then:
+my $last_non_blank_line;
+while (my $line = readline $fh) {
+    # choose one of the following two lines, depending what you meant
+    if ( $line =~ /\S/ ) { $last_non_blank_line = $line }
+    # if ( line !~ /^$/ ) { $last_non_blank_line = $line }
+}
+
+print "$last_non_blank_line";
+}
