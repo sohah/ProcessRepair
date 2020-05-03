e41ecd8212a1786fb545a57c53b278b783f83a12
sohah <husseinsoh@gmail.com>
2020-05-03 07:28:20 -0500

    adding perl script that consolidates all properties into one depending on the benchmark

--- makeLustreBody.pl
+++ makeLustreBody.pl
@@ -1,24 +1,86 @@
 #!/usr/bin/perl
-
-$filename = $ARGV[0];
-
-open(FILE, $filename) or die "Could not read from $filename, program halting.";
+use strict; use warnings;
 
 
-# gpca counter needs to start from 10, since 0-9 are the already existing properties that we want to see if we have met them while trying to repair others.
-# infusion counter needs to start from 12 since 0-11 are the already existing properties that we want to see if we have met them while trying to repair others. Note that for p4 and p14 are taken out and so p4 is actually p5 and p5 is actually p6 and so on. Since we cannot check with invalid prop.
+=pod
+This scripts takes as an input (1) filenamae of the set of tighter properties we have found. (2) name of the benchmark "infusion, tcas, wbs, or gpca".
+Also, it assumes the existance of shellFiles, and OrigProps files for each benchmark to operate.
+The outut is a spec defined in lustre.
+=cut
 
-my $counter = 12;
+
+my $tightPropCounter;
+my @invalidProp = (0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+my $origPropFileName;
+my $shellFileName;
+my $spec = $ARGV[1];
+
+if($spec eq "infusion"){
+    $invalidProp[4] = 1;
+    $invalidProp[14] = 1;
+    $tightPropCounter = 15;
+    $origPropFileName="props/InfusionOrigProps";
+    $shellFileName="props/InfusionShell";
+} elsif($spec eq "tcas"){
+    $invalidProp[3] = 1;
+    $tightPropCounter = 12;
+    $origPropFileName="props/TCASOrigProps";
+    $shellFileName="props/TCASShell";
+} elsif($spec eq "wbs"){
+    $invalidProp[2] = 1;
+    $invalidProp[4] = 1;
+    $invalidProp[5] = 1;
+    $tightPropCounter = 12;
+    $origPropFileName="props/WBSOrigProps";
+    $shellFileName="props/WBSShell";
+} else { #must be the gpca
+    $tightPropCounter = 11;
+    $origPropFileName="props/AlarmOrigProps";
+    $shellFileName="props/AlarmShell";
+}
+
+my @tightPropsStr;
+my @declarations;
+my $tightPropFileName = $ARGV[0];
+
+open(FILE, $tightPropFileName) or die "Could not read from $tightPropFileName, program halting.";
+
 while(my $line = <FILE>)
 {
   $line =~ s/\s+$//;
   my $prop = substr $line, 3, -1;
-  print "p$counter";
-  print "$prop\n";
-  ++$counter;
+  push(@tightPropsStr,"p$tightPropCounter");
+  push(@tightPropsStr,"$prop\n");
+  #print "p$tightPropCounter";
+  #print "$prop\n";
+  ++$tightPropCounter;
 }
-
-for ( my $i = 1; $i < $counter; $i++ ) {
-    print"p$i : bool;\n";
-  }
 close FILE;
+
+
+
+for ( my $i = 0; $i < $tightPropCounter; $i++ ) {
+    if($i<=13 && $invalidProp[$i] != 1){
+        push(@declarations,"p$i : bool;\n");}
+  }
+
+
+local $/;
+open(FILE, $shellFileName) or die "Can't read file 'shellFileName' [$!]\n";
+my $shellFile = <FILE>;
+close (FILE);
+
+
+local $/;
+open(FILE, $origPropFileName) or die "Can't read file 'origPropFileName' [$!]\n";
+my $origPropFile = <FILE>;
+close (FILE);
+
+
+print $shellFile;
+print "\n@declarations";
+print "let\n";
+print "$origPropFile";
+print "\n";
+print @tightPropsStr;
+print "tel;\n";
