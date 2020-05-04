4c4c69c05977aff6b0f3e6cb33cfad20f7eee3cb
sohah <husseinsoh@gmail.com>
2020-05-04 16:49:21 -0500

    pushing edits on shell scripts to rename to the name of props

--- executeMakeBody.sh
+++ executeMakeBody.sh
@@ -1,37 +1,37 @@
 
 
-perl makeLustreBody.pl props/tightProp/gpca_Prop1.txt  gpca > props/Body/gpca_Prop1.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop2.txt  gpca > props/Body/gpca_Prop2.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop3.txt  gpca > props/Body/gpca_Prop3.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop4.txt  gpca > props/Body/gpca_Prop4.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop5.txt  gpca > props/Body/gpca_Prop5.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop6.txt  gpca > props/Body/gpca_Prop6.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop7.txt  gpca > props/Body/gpca_Prop7.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop8.txt  gpca > props/Body/gpca_Prop8.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop9.txt  gpca > props/Body/gpca_Prop9.lus
-perl makeLustreBody.pl props/tightProp/gpca_Prop10.txt  gpca > props/Body/gpca_Prop10.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop1.txt  gpca > props/Body/gpca_prop1.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop2.txt  gpca > props/Body/gpca_prop2.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop3.txt  gpca > props/Body/gpca_prop3.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop4.txt  gpca > props/Body/gpca_prop4.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop5.txt  gpca > props/Body/gpca_prop5.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop6.txt  gpca > props/Body/gpca_prop6.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop7.txt  gpca > props/Body/gpca_prop7.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop8.txt  gpca > props/Body/gpca_prop8.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop9.txt  gpca > props/Body/gpca_prop9.lus
+perl makeLustreBody.pl props/tightProp/gpca_prop10.txt  gpca > props/Body/gpca_prop10.lus
 
 
-perl makeLustreBody.pl props/tightProp/infusion_Prop1.txt  infusion > props/Body/infusion_prop1.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop2.txt  infusion > props/Body/infusion_prop2.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop3.txt  infusion > props/Body/infusion_prop3.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop5.txt  infusion > props/Body/infusion_prop5.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop6.txt  infusion > props/Body/infusion_prop6.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop7.txt  infusion > props/Body/infusion_prop7.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop8.txt  infusion > props/Body/infusion_prop8.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop9.txt  infusion > props/Body/infusion_prop9.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop10.txt  infusion > props/Body/infusion_prop10.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop11.txt  infusion > props/Body/infusion_prop11.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop12.txt  infusion > props/Body/infusion_prop12.lus
-perl makeLustreBody.pl props/tightProp/infusion_Prop13.txt  infusion > props/Body/infusion_prop13.lus
-
-
-
-perl makeLustreBody.pl props/tightProp/tcas_Prop1.txt  tcas > props/Body/tcas_prop1.lus
-perl makeLustreBody.pl props/tightProp/tcas_Prop1.txt  tcas > props/Body/tcas_prop2.lus
-perl makeLustreBody.pl props/tightProp/tcas_Prop1.txt  tcas > props/Body/tcas_prop4.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop1.txt  infusion > props/Body/infusion_prop1.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop2.txt  infusion > props/Body/infusion_prop2.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop3.txt  infusion > props/Body/infusion_prop3.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop5.txt  infusion > props/Body/infusion_prop5.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop6.txt  infusion > props/Body/infusion_prop6.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop7.txt  infusion > props/Body/infusion_prop7.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop8.txt  infusion > props/Body/infusion_prop8.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop9.txt  infusion > props/Body/infusion_prop9.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop10.txt  infusion > props/Body/infusion_prop10.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop11.txt  infusion > props/Body/infusion_prop11.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop12.txt  infusion > props/Body/infusion_prop12.lus
+perl makeLustreBody.pl props/tightProp/infusion_prop13.txt  infusion > props/Body/infusion_prop13.lus
 
 
 
-perl makeLustreBody.pl props/tightProp/wbs_Prop1.txt  wbs > props/Body/wbs_prop1.lus
-perl makeLustreBody.pl props/tightProp/wbs_Prop3.txt  wbs > props/Body/wbs_prop3.lus
+perl makeLustreBody.pl props/tightProp/tcas_prop1.txt  tcas > props/Body/tcas_prop1.lus
+perl makeLustreBody.pl props/tightProp/tcas_prop1.txt  tcas > props/Body/tcas_prop2.lus
+perl makeLustreBody.pl props/tightProp/tcas_prop1.txt  tcas > props/Body/tcas_prop4.lus
+
+
+
+perl makeLustreBody.pl props/tightProp/wbs_prop1.txt  wbs > props/Body/wbs_prop1.lus
+perl makeLustreBody.pl props/tightProp/wbs_prop3.txt  wbs > props/Body/wbs_prop3.lus
---
+++ renameOutput.sh
@@ -0,0 +1,65 @@
+#used to rename output to what the makeLustreBody is waiting for. It should be executed on workPlayGround on the server, then resulting files should be moved to ProcessRepair project for processing
+
+cp ./DiscoveryExamples/GPCA_Alarm/Prop1/gpca_all_tightProps.txt ./gpca_prop1.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop2/gpca_all_tightProps.txt ./gpca_prop2.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop3/gpca_all_tightProps.txt ./gpca_prop3.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop4/gpca_all_tightProps.txt ./gpca_prop4.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop5/gpca_all_tightProps.txt ./gpca_prop5.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop6/gpca_all_tightProps.txt ./gpca_prop6.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop7/gpca_all_tightProps.txt ./gpca_prop7.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop8/gpca_all_tightProps.txt ./gpca_prop8.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop9/gpca_all_tightProps.txt ./gpca_prop9.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop10/gpca_all_tightProps.txt ./gpca_prop10.txt
+
+cp ./DiscoveryExamples/GPCA_Infusion/Prop1/infusion_all_tightProps.txt ./infusion_prop1.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop2/infusion_all_tightProps.txt ./infusion_prop2.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop3/infusion_all_tightProps.txt ./infusion_prop3.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop5/infusion_all_tightProps.txt ./infusion_prop5.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop6/infusion_all_tightProps.txt ./infusion_prop6.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop7/infusion_all_tightProps.txt ./infusion_prop7.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop8/infusion_all_tightProps.txt ./infusion_prop8.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop9/infusion_all_tightProps.txt ./infusion_prop9.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop10/infusion_all_tightProps.txt ./infusion_prop10.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop11/infusion_all_tightProps.txt ./infusion_prop11.txt
+
+
+cp ./DiscoveryExamples/TCAS/Prop1/tcas_all_tightProps.txt ./tcas_prop1.txt
+cp ./DiscoveryExamples/TCAS/Prop2/tcas_all_tightProps.txt ./tcas_prop2.txt
+cp ./DiscoveryExamples/TCAS/Prop4/tcas_all_tightProps.txt ./tcas_prop4.txt
+
+cp ./DiscoveryExamples/WBS/Prop1/wbs_all_tightProps.txt ./wbs_prop1.txt
+cp ./DiscoveryExamples/WBS/Prop3/wbs_all_tightProps.txt ./wbs_prop3.txt
+
+
+
+
+
+cp ./DiscoveryExamples/GPCA_Alarm/Prop1/gpca_all_stats.txt ./gpca_prop1_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop2/gpca_all_stats.txt ./gpca_prop2_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop3/gpca_all_stats.txt ./gpca_prop3_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop4/gpca_all_stats.txt ./gpca_prop4_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop5/gpca_all_stats.txt ./gpca_prop5_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop6/gpca_all_stats.txt ./gpca_prop6_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop7/gpca_all_stats.txt ./gpca_prop7_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop8/gpca_all_stats.txt ./gpca_prop8_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop9/gpca_all_stats.txt ./gpca_prop9_stats.txt
+cp ./DiscoveryExamples/GPCA_Alarm/Prop10/gpca_all_stats.txt ./gpca_prop10_stats.txt
+
+cp ./DiscoveryExamples/GPCA_Infusion/Prop1/infusion_all_stats.txt ./infusion_prop1_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop2/infusion_all_stats.txt ./infusion_prop2_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop3/infusion_all_stats.txt ./infusion_prop3_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop5/infusion_all_stats.txt ./infusion_prop5_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop6/infusion_all_stats.txt ./infusion_prop6_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop7/infusion_all_stats.txt ./infusion_prop7_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop8/infusion_all_stats.txt ./infusion_prop8_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop9/infusion_all_stats.txt ./infusion_prop9_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop10/infusion_all_stats.txt ./infusion_prop10_stats.txt
+cp ./DiscoveryExamples/GPCA_Infusion/Prop11/infusion_all_stats.txt ./infusion_prop11_stats.txt
+
+
+cp ./DiscoveryExamples/TCAS/Prop1/tcas_all_stats.txt ./tcas_prop1_stats.txt
+cp ./DiscoveryExamples/TCAS/Prop2/tcas_all_stats.txt ./tcas_prop2_stats.txt
+cp ./DiscoveryExamples/TCAS/Prop4/tcas_all_stats.txt ./tcas_prop4_stats.txt
+
+cp ./DiscoveryExamples/WBS/Prop1/wbs_all_stats.txt ./wbs_prop1_stats.txt
+cp ./DiscoveryExamples/WBS/Prop3/wbs_all_stats.txt ./wbs_prop3_stats.txt
