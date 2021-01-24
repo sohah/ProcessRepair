123047971c923e9bb458452df7b56533b9316fc4
sohah <husse200@umn.edu>
2021-01-24 15:03:02 -0600

    adding stat files for expirements

---
+++ props/Multi-Mutation_2Mutation_False/stats/multipleCommands.sh
@@ -0,0 +1,11 @@
+#used that to write commands that I want to run on multiple expirements at a time.
+#!/usr/bin/env bash
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/WBS/*/wbs_prop*_stats.txt .
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/TCAS/*/tcas_prop*_stats.txt .
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/GPCA_Infusion/*/infusion_prop*_stats.txt .
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/GPCA_Alarm/*/gpca_prop*_stats.txt .
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/WBS/*/wbs_prop*_stats.txt ../../Multi-Mutation_2Mutation_True/stats/
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/TCAS/*/tcas_prop*_stats.txt ../../Multi-Mutation_2Mutation_True/stats/
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/TCAS/*/tcas_prop*_stats.txt ../../Multi-Mutation_2Mutation_True/stats/
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/GPCA_Infusion/*/infusion_prop*_stats.txt ../../Multi-Mutation_2Mutation_True/stats/
+cp /media/soha/DATA/MultiMutationExpr/2Mutation/ranger-discovery/src/DiscoveryExamples/GPCA_Alarm/*/gpca_prop*_stats.txt ../../Multi-Mutation_2Mutation_True/stats/
