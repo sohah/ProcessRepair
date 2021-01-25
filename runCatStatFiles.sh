435f4734c2304f2b1713ee532e70e32fb85b8357
sohah <husse200@umn.edu>
2021-01-25 06:19:18 -0600

    adding all tautology analysis files and the update the path for the runCatStatFiles.sh

--- runCatStatFiles.sh
+++ runCatStatFiles.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-cat $1/stats/gpca_*noTautStatFile.txt > $1/splitStats/gpca_noTautStat.txt
-cat $1/stats/gpca_*tautStatFile.txt > $1/splitStats/gpca_tautStat.txt
-cat $1/stats/gpca_*tautOtherStatFile.txt > $1/splitStats/gpca_tautOtherStat.txt
+cat $1/splitStats/gpca_*noTautStatFile.txt > $1/splitStats/gpca_noTautStat.txt
+cat $1/splitStats/gpca_*tautStatFile.txt > $1/splitStats/gpca_tautStat.txt
+cat $1/splitStats/gpca_*tautOtherStatFile.txt > $1/splitStats/gpca_tautOtherStat.txt
 
-cat $1/stats/infusion_*noTautStatFile.txt > $1/splitStats/infusion_noTautStat.txt
-cat $1/stats/infusion_*tautStatFile.txt > $1/splitStats/infusion_tautStat.txt
-cat $1/stats/infusion_*tautOtherStatFile.txt > $1/splitStats/infusion_tautOtherStat.txt
+cat $1/splitStats/infusion_*noTautStatFile.txt > $1/splitStats/infusion_noTautStat.txt
+cat $1/splitStats/infusion_*tautStatFile.txt > $1/splitStats/infusion_tautStat.txt
+cat $1/splitStats/infusion_*tautOtherStatFile.txt > $1/splitStats/infusion_tautOtherStat.txt
 
-cat $1/stats/tcas_*noTautStatFile.txt > $1/splitStats/tcas_noTautStat.txt
-cat $1/stats/tcas_*tautStatFile.txt > $1/splitStats/tcas_tautStat.txt
-cat $1/stats/tcas_*tautOtherStatFile.txt > $1/splitStats/tcas_tautOtherStat.txt
+cat $1/splitStats/tcas_*noTautStatFile.txt > $1/splitStats/tcas_noTautStat.txt
+cat $1/splitStats/tcas_*tautStatFile.txt > $1/splitStats/tcas_tautStat.txt
+cat $1/splitStats/tcas_*tautOtherStatFile.txt > $1/splitStats/tcas_tautOtherStat.txt
 
-cat $1/stats/wbs_*noTautStatFile.txt > $1/splitStats/wbs_noTautStat.txt
-cat $1/stats/wbs_*tautStatFile.txt > $1/splitStats/wbs_tautStat.txt
-cat $1/stats/wbs_*tautOtherStatFile.txt > $1/splitStats/wbs_tautOtherStat.txt
+cat $1/splitStats/wbs_*noTautStatFile.txt > $1/splitStats/wbs_noTautStat.txt
+cat $1/splitStats/wbs_*tautStatFile.txt > $1/splitStats/wbs_tautStat.txt
+cat $1/splitStats/wbs_*tautOtherStatFile.txt > $1/splitStats/wbs_tautOtherStat.txt
